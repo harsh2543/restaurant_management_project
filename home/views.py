3bcb7d6ba77eb4dcce9e4bba80f752903bdceee1
Harsh Sheth <harshetsheth1@gmail.com>
2025-08-20 12:38:07 +0530

    23519

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def home_view(request):
     return render(request, "home.html", {"restaurant_name": "Django Bites"})
+
+def home_view2(request):
+    return render(request, "home.html", {"phone": settings.RESTAURANT_PHONE})
